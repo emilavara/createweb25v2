82bb83f57f45a3f5138ace46c7ef8333ed59adee
emilavara <emilavara@hotmail.com>
2025-09-26 23:59:09 +0200

    started work on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,45 @@
 </script>
 
 <Header/>
-<Section style="height: calc(100vh - 4rem + 1px); padding-top: 4rem;" lines="X---X">
-    <Column span={{xs: 6, lg: 12}}>
-        <h1>Hem</h1>
+<Section class="pt-4" lines="XXXXX">
+    <Column span={6} class="pt-4 px-1">
+        <h1 class="lowercase">Vi skapar oförglömliga digitala upplevelser</h1>
+        <p class="hero mt-2">Vi är en Stockholm-baserad digitalbyrå mer flera år <br> av erfarenhet av att skapa banbrytande applikationer <br> och webbplatser.</p>
+        <div class="button-container mt-10">
+            <div class="button-test">
+                <div>Button 1</div>
+                <div>-></div>
+            </div>
+            <div class="button-test">
+                <div>Button 2</div>
+                <div>-></div>
+            </div>
+        </div>
+    </Column>
+</Section>
+<Section class="py-8" lines="XXXXX" borderTop={true}>
+    <Column span={6} class="py-4 px-1">
+        <p class="subheading text-accent">Det här kan vi</p>
+        <h2>Createweb i ett <br/> nötskal</h2>
     </Column>
 </Section>
 <Footer/>
+
+<style lang="scss">
+    .button-test {
+        width: calc(50% + 1rem);
+        border-top: 1px solid var(--border-color);
+        border-bottom: 1px solid var(--border-color);
+        padding: 1.5rem;
+        margin-left: -1rem;
+        font-weight: 600;
+        text-transform: uppercase;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        &:last-of-type {
+            border: none;
+        }
+    }
+</style>
